09aeb78e0c1311c57738e313f437c8027ae54c41
cystal-dot <jiniaen4@gmail.com>
2024-01-18 22:46:21 +0900

    API経由でデータを引っ張れるように

--- client/src/app/_common/list.tsx
+++ client/src/app/_common/list.tsx
@@ -38,7 +38,7 @@ const products = [
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
   },
   {
-    id: 4,
+    id: 5,
     name: "Machined Mechanical Pencil",
     href: "#",
     price: "$35",
